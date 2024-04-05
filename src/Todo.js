89f6ab38cb38abdc1fbe7b25eff705166f9bf932
hwangsungmin <hwangsungmin_00@naver.com>
2024-04-05 22:25:19 +0900

    기본 설정

---
+++ src/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Todo = () =>{
+    return (
+        <>
+        <div className="Todo">
+            <input type="checkbox" id="todo0" name="todo0" value="todo0"></input>
+            <label for="todo0">Todo 컴포넌트 만들기</label>
+        </div>
+        <h1>성미니</h1>
+        </>
+    );
+};
+
+export default Todo;
