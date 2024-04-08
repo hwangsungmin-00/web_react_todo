ccf3598e17f480ae4c223a161a7b4409b67291a9
hwangsungmin <hwangsungmin_00@naver.com>
2024-04-08 16:13:02 +0900

    props와 useState 훅 이용한 todoItems 생성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 import Todo from './Todo';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [items, setItems] = useState([{
+    id:"0",
+    title:"Hello World 1",
+    done: true
+  },
+  {
+    id:"1",
+    title:"Hello World 2",
+    done: false
+  },
+  {
+    id:"2",
+    title:"Hello World 3",
+    done: false
+  }]);
+
+  //JSX 결과를 변수에 저장
+  let todoItems =items.length > 0 && items.map((item) => <Todo item={item} key={item.id}/>);
+
+  //변수를 반환
   return (
+    //<div className="App">
+    //  <Todo item="공부하기"/>
+    //  <Todo item="잠자기"/>
+    //  <Todo item="운동하기"/>
+    //</div>
     <div className="App">
-      <Todo/>
-      <Todo/>
-      <Todo/>
-      <Todo/>
+      {/*
+      <Todo item={items[0]}/>
+  <Todo item={items[1]}/>*/}
+  {todoItems}
     </div>
   );
 }
--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Todo = () =>{
+const Todo = (props) =>{
+    // let item = props.item;
+
+    const [item, setItem] = useState(props.item);
+
+    //let output= <div className="Todo"> {/* 주석 */}
+    //                <input type="checkbox" id="todo0" name="todo0" value="todo0"></input>
+    //                <label for="todo0">{item.id}{item.title}</label>
+    //            </div>;
     return (
-        <>
-        <div className="Todo">
-            <input type="checkbox" id="todo0" name="todo0" value="todo0"></input>
-            <label for="todo0">Todo 컴포넌트 만들기</label>
+        <div className='Todo'>
+            <input
+            type="checkbox"
+            id={item.id}
+            name={item.id}
+            checked={item.done}
+            />
+            <label id={item.id}>{item.title}</label>
         </div>
-        <h1>성미니</h1>
-        </>
     );
 };
 
