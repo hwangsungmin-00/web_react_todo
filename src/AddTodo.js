6c2733f5b9356b8d5bc000b9137be086e0e18340
hwangsungmin <hwangsungmin_00@naver.com>
2024-04-15 16:13:51 +0900

    Todo 추가, 삭제, 수정 기능 구현

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -3,7 +3,19 @@ import {Button, Grid, TextField} from "@mui/material";
 
 const AddTodo = (props) => {
     const [item, setItem] = useState({title: ""});
+    const addItem = props.addItem;
 
+    //onButtonClick 함수 작성
+    const onButtonClick = ()=>{
+        addItem(item);
+        setItem({title:""});
+    }
+    //enterKeyEventHandler 함수
+    const enterKeyEventHandler=(e)=>{
+        if(e.key=='Enter'){
+            onButtonClick();
+        }
+    }
 
     const onInputChange = (e) => {
         setItem({title: e.target.value}); // item 상태 변수 값이 바뀌고 자동으로 리렌더링이 일어남
@@ -14,10 +26,12 @@ const AddTodo = (props) => {
         <Grid container style={{marginTop:20}}>
             <Grid xs={11} md={11} item style={{paddingRight:16}}>
                 <TextField placeholder="Add Todo here" fullWidth 
-                onChange={onInputChange} value={item.title} />
+                onChange={onInputChange} 
+                onKeyPress={enterKeyEventHandler}
+                value={item.title} />
             </Grid>
             <Grid xs={1} md={1} item>
-                <Button fullWidth style={{height:'100%'}} color="secondary" variant="outlined">
+                <Button fullWidth style={{height:'100%'}} color="secondary" variant="outlined" onClick={onButtonClick}>
                     +
                 </Button>
             </Grid>
--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,35 @@
 import React, { useState } from 'react';
-import { ListItem, ListItemText, InputBase, Checkbox } from "@mui/material";
+import { ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton } from "@mui/material";
+import DeleteOutlined from "@mui/icons-material/DeleteOutlined"
 
 const Todo = (props) =>{
     // let item = props.item;
 
     const [item, setItem] = useState(props.item);
+    const deleteItem = props.deleteItem
+    const [readOnly, setReadOnly] = useState(true);
+    const editItem = props.editItem;
+
+    // turnOffReadOnly 함수 작성 
+    const turnOffReadOnly=()=>{
+        setReadOnly(false);
+    }
+
+    // turnOnReadOnly 함수 작성 
+    const turnOnReadOnly=(e)=>{
+        if(e.key=="Enter"){
+            setReadOnly(true);
+        }
+    }
+
+    const deleteEventHandler = () => {
+        deleteItem(item);
+    };
+
+    const editEventHandler = (e)=>{
+        item.title = e.target.value;
+        editItem();
+    }
 
     //let output= <div className="Todo"> {/* 주석 */}
     //                <input type="checkbox" id="todo0" name="todo0" value="todo0"></input>
@@ -26,7 +51,12 @@ const Todo = (props) =>{
             <Checkbox checked={item.done} />
             <ListItemText>
                 <InputBase
-                    inputProps={{"aria-label": "naked"}}
+                    inputProps={{
+                        "aria-label": "naked", 
+                        readOnly: readOnly}}
+                    onClick={turnOffReadOnly}
+                    onKeyDown={turnOnReadOnly}
+                    onChange={editEventHandler}
                     type="text"
                     id={item.id}
                     name={item.id}
@@ -35,6 +65,12 @@ const Todo = (props) =>{
                     fullWidth={true}
                 />
             </ListItemText>
+            <ListItemSecondaryAction>
+                <IconButton aria-label="Delete Todo"
+                onClick={deleteEventHandler}>
+                    <DeleteOutlined />
+                </IconButton>
+            </ListItemSecondaryAction>
         </ListItem>
     ) 
 };
